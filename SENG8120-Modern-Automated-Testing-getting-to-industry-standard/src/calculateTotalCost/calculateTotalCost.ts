dd8d9462cfef8d5a61eeca583991b7718212aab1
Humera121 <humeramalvat21@gmail.com>
2025-07-20 11:36:40 -0400

    fix: add validation to total cost calculation

--- SENG8120-Modern-Automated-Testing-getting-to-industry-standard/src/calculateTotalCost/calculateTotalCost.ts
+++ SENG8120-Modern-Automated-Testing-getting-to-industry-standard/src/calculateTotalCost/calculateTotalCost.ts
@@ -1,3 +1,11 @@
-export default function calculateTotalCost(paintCost: number, laborCost: number): number {
+export default function calculateTotalCost(
+  paintCost: number,
+  laborCost: number
+): number {
+  if (paintCost < 0 || laborCost < 0) {
+    throw new Error('Paint cost and labor cost must be non-negative.');
+  }
+
   return paintCost + laborCost;
 }
+
