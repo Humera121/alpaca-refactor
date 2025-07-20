c7f3a098ec8ebb753fb4e91d5be5e3594b0f2faa
Humera121 <humeramalvat21@gmail.com>
2025-07-20 19:05:46 -0400

    fix: update import paths and type usage in main and index files

--- SENG8120-Modern-Automated-Testing-getting-to-industry-standard/src/calculateCanvasPerimeter/index.ts
+++ SENG8120-Modern-Automated-Testing-getting-to-industry-standard/src/calculateCanvasPerimeter/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./calculateCanvasSize";
+import calculateCanvasSize from "./calculateCanvasSize";
--- SENG8120-Modern-Automated-Testing-getting-to-industry-standard/src/main.ts
+++ SENG8120-Modern-Automated-Testing-getting-to-industry-standard/src/main.ts
@@ -1,8 +1,8 @@
-import calculateCanvasSize from "./calculateCanvasSize";
-import paintRequiredCalculator from "./paintRequiredCalculator";
+import calculateCanvasSize from "./calculateCanvasSize/calculateCanvasSize";
+import paintRequiredCalculator from "./paintRequiredCalculator/paintRequiredCalculator";
 
 function main() {
-  const area = calculateCanvasSize("10", "20");
+  const area = calculateCanvasSize(10, 20);
   const coveragePerLiter = 11.4;
 
   const paintRequired = paintRequiredCalculator(area, coveragePerLiter);
